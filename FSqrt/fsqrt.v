e4d4a572a56ede4cce34d72bbdd910c0e3324199
Noahz-Ark <tk.noahzark@gmail.com>
2020-02-26 17:34:32 +0900

    divide fsqrt into 7 parts

--- FSqrt/fsqrt.v
+++ FSqrt/fsqrt.v
@@ -2,9 +2,9 @@
 module fsqrt_stage1(
     input wire [31:0] s,
     output wire [63:0] target,
-    output reg [63:0] b1,
-    output reg [63:0] c1,
-    output reg [63:0] d1
+    output wire [63:0] b1,
+    output wire [63:0] c1,
+    output wire [63:0] d1
 );
 
 // 符号1bit、指数8bit、仮数23bitを読み出す
@@ -302,27 +302,34 @@ endmodule
 
 // NOTE: stage2
 module fsqrt_stage2(
-    input wire [63:0] target,
-    input wire [63:0] b1,
     input wire [63:0] c1,
     input wire [63:0] d1,
-    output reg [63:0] x1
+    output wire [63:0] e1
 );
 
-wire [63:0] e1;
 assign e1 = (c1 * d1) >> 8'd32; // e = ax^3/2
+
+endmodule
+
+// NOTE: stage3
+module fsqrt_stage3(
+    input wire [63:0] b1,
+    input wire [63:0] e1,
+    output wire [63:0] x1
+);
+
 assign x1 = b1 - e1;   // x' = (3x-ax^3)/2
 
 endmodule
 
 
-// NOTE: stage3
-module fsqrt_stage3(
-    input reg [63:0] target,
-    input reg [63:0] x1,
-    output reg [63:0] b2,
-    output reg [63:0] c2,
-    output reg [63:0] d2
+// NOTE: stage4
+module fsqrt_stage4(
+    input wire [63:0] target,
+    input wire [63:0] x1,
+    output wire [63:0] b2,
+    output wire [63:0] c2,
+    output wire [63:0] d2
 );
 
 // NOTE: Newton法を回す x_{n+1} = 1/2 (3 x_n + a / x_n)
@@ -335,13 +342,34 @@ assign d2 = (x1 * x1) >> 8'd31; // d = x^2/2
 endmodule
 
 
-// NOTE: stage4
-module fsqrt_stage4(
-    input wire [31:0] s,
-    input wire [63:0] target,
-    input wire [63:0] b2,
+// NOTE: stage5
+module fsqrt_stage5(
     input wire [63:0] c2,
     input wire [63:0] d2,
+    output wire [63:0] e2
+);
+
+assign e2 = (c2 * d2) >> 8'd32; // e = ax^3/2
+
+endmodule
+
+
+// NOTE: stage6
+module fsqrt_stage6(
+    input wire [63:0] b2,
+    input wire [63:0] e2,
+    output wire [63:0] x2
+);
+
+assign x2 = b2 - e2;   // x' = (3x-ax^3)/2
+
+endmodule
+
+// NOTE: stage7
+module fsqrt_stage7(
+    input wire [31:0] s,
+    input wire [63:0] target,
+    input wire [63:0] x2,
     output wire [31:0] d
 );
 
@@ -354,9 +382,7 @@ assign sign_s = s[31:31];
 assign exponent_s = s[30:23];
 assign mantissa_s = s[22:0];
 
-wire [63:0] e2, x2, y2;
-assign e2 = (c2 * d2) >> 8'd32; // e = ax^3/2
-assign x2 = b2 - e2;   // x' = (3x-ax^3)/2
+wire [63:0] y2;
 assign y2 = (x2 * target) >> 8'd31;
 
 // 符号を決める
@@ -396,18 +422,20 @@ module fsqrt(
   output wire [31:0] d
 );
 
-wire [31:0] wire_s1, wire_s2, wire_s3, wire_s4;
-wire [63:0] wire_target1, wire_target2, wire_target3, wire_target4;
-wire [63:0] wire_b1, wire_b2, wire_b3, wire_b4;
-wire [63:0] wire_c1, wire_c2, wire_c3, wire_c4;
-wire [63:0] wire_d1, wire_d2, wire_d3, wire_d4;
-wire [63:0] wire_x1, wire_x2, wire_x3, wire_x4;
-reg [31:0] reg_s1, reg_s2, reg_s3, reg_s4;
-reg [63:0] reg_target1, reg_target2, reg_target3, reg_target4;
-reg [63:0] reg_b1, reg_b2, reg_b3, reg_b4;
-reg [63:0] reg_c1, reg_c2, reg_c3, reg_c4;
-reg [63:0] reg_d1, reg_d2, reg_d3, reg_d4;
-reg [63:0] reg_x1, reg_x2, reg_x3, reg_x4;
+wire [31:0] wire_s1, wire_s2, wire_s3, wire_s4, wire_s5, wire_s6, wire_s7;
+wire [63:0] wire_target1, wire_target2, wire_target3, wire_target4, wire_target5, wire_target6, wire_target7;
+wire [63:0] wire_b1, wire_b2, wire_b3, wire_b4, wire_b5, wire_b6;
+wire [63:0] wire_c1, wire_c2, wire_c4, wire_c5;
+wire [63:0] wire_d1, wire_d2, wire_d4, wire_d5;
+wire [63:0] wire_e2, wire_e3, wire_e5, wire_e6;
+wire [63:0] wire_x3, wire_x4, wire_x5, wire_x6, wire_x7;
+reg [31:0] reg_s1, reg_s2, reg_s3, reg_s4, reg_s5, reg_s6, reg_s7;
+reg [63:0] reg_target1, reg_target2, reg_target3, reg_target4, reg_target5, reg_target6, reg_target7;
+reg [63:0] reg_b1, reg_b2, reg_b3, reg_b4, reg_b5, reg_b6;
+reg [63:0] reg_c2, reg_c5;
+reg [63:0] reg_d2, reg_d5;
+reg [63:0] reg_e2, reg_e3, reg_e5, reg_e6;
+reg [63:0] reg_x4, reg_x5, reg_x7;
 
 /*
 NOTE: Signature
@@ -419,68 +447,107 @@ module fsqrt_stage1(
     output reg [63:0] d1
 );
 module fsqrt_stage2(
-    input wire [63:0] target,
-    input wire [63:0] b1,
     input wire [63:0] c1,
     input wire [63:0] d1,
-    output reg [63:0] x1
+    output wire [63:0] e1
 );
+
 module fsqrt_stage3(
-    input reg [63:0] target,
-    input reg [63:0] x1,
-    output reg [63:0] b2,
-    output reg [63:0] c2,
-    output reg [63:0] d2
+    input wire [63:0] b1,
+    input wire [63:0] e1,
+    output wire [63:0] x1
 );
+
 module fsqrt_stage4(
-    input wire [31:0] s,
     input wire [63:0] target,
-    input wire [63:0] b2,
+    input wire [63:0] x1,
+    output wire [63:0] b2,
+    output wire [63:0] c2,
+    output wire [63:0] d2
+);
+
+module fsqrt_stage5(
     input wire [63:0] c2,
     input wire [63:0] d2,
+    output wire [63:0] e2
+);
+
+module fsqrt_stage6(
+    input wire [63:0] b2,
+    input wire [63:0] e2,
+    output wire [63:0] x2
+);
+
+module fsqrt_stage7(
+    input wire [31:0] s,
+    input wire [63:0] target,
+    input wire [63:0] x2,
     output wire [31:0] d
 );
 */
-fsqrt_stage1 u1(s,wire_target1,wire_b1,wire_c1,wire_d1);
-fsqrt_stage2 u2(wire_target2,wire_b2,wire_c2,wire_d2,wire_x2);
-fsqrt_stage3 u3(wire_target3,wire_x3,wire_b3,wire_c3,wire_d3);
-fsqrt_stage4 u4(wire_s4,wire_target4,wire_b4,wire_c4,wire_d4,d);
+
+
+fsqrt_stage1 u1(wire_s1,wire_target1,wire_b1,wire_c1,wire_d1);
+fsqrt_stage2 u2(wire_c2,wire_d2,wire_e2);
+fsqrt_stage3 u3(wire_b3,wire_e3,wire_x3);
+fsqrt_stage4 u4(wire_target4,wire_x4,wire_b4,wire_c4,wire_d4);
+fsqrt_stage5 u5(wire_c5,wire_d5,wire_e5);
+fsqrt_stage6 u6(wire_b6,wire_e6,wire_x6);
+fsqrt_stage7 u7(wire_s7,wire_target7,wire_x7,d);
 
 assign wire_s1 = s;
 assign wire_s2 = reg_s2;
 assign wire_s3 = reg_s3;
 assign wire_s4 = reg_s4;
+assign wire_s5 = reg_s5;
+assign wire_s6 = reg_s6;
+assign wire_s7 = reg_s7;
 assign wire_target2 = reg_target2;
 assign wire_target3 = reg_target3;
 assign wire_target4 = reg_target4;
+assign wire_target5 = reg_target5;
+assign wire_target6 = reg_target6;
+assign wire_target7 = reg_target7;
 assign wire_b2 = reg_b2;
-assign wire_b4 = reg_b4;
+assign wire_b3 = reg_b3;
+assign wire_b5 = reg_b5;
+assign wire_b6 = reg_b6;
 assign wire_c2 = reg_c2;
-assign wire_c4 = reg_c4;
+assign wire_c5 = reg_c5;
 assign wire_d2 = reg_d2;
-assign wire_d4 = reg_d4;
-assign wire_x3 = reg_x3;
+assign wire_d5 = reg_d5;
+assign wire_e3 = reg_e3;
+assign wire_e6 = reg_e6;
+assign wire_x4 = reg_x4;
+assign wire_x5 = reg_x5;
+assign wire_x7 = reg_x7;
 
 always @(posedge clk) begin
-  reg_s4 <= wire_s3;
-end
-always @(posedge clk) begin
-  reg_s3 <= wire_s2;
-end
-always @(posedge clk) begin
   reg_s2 <= wire_s1;
-end
-always @(posedge clk) begin
+  reg_s3 <= wire_s2;
+  reg_s4 <= wire_s3;
+  reg_s5 <= wire_s4;
+  reg_s6 <= wire_s5;
+  reg_s7 <= wire_s6;
   reg_target2 <= wire_target1;
   reg_target3 <= wire_target2;
   reg_target4 <= wire_target3;
+  reg_target5 <= wire_target4;
+  reg_target6 <= wire_target5;
+  reg_target7 <= wire_target6;
   reg_b2 <= wire_b1;
+  reg_b3 <= wire_b2;
+  reg_b5 <= wire_b4;
+  reg_b6 <= wire_b5;
   reg_c2 <= wire_c1;
+  reg_c5 <= wire_c4;
   reg_d2 <= wire_d1;
-  reg_x3 <= wire_x2;
-  reg_b4 <= wire_b3;
-  reg_c4 <= wire_c3;
-  reg_d4 <= wire_d3;
+  reg_d5 <= wire_d4;
+  reg_e3 <= wire_e2;
+  reg_e6 <= wire_e5;
+  reg_x4 <= wire_x3;
+  reg_x5 <= wire_x4;
+  reg_x7 <= wire_x6;
 end
 
 endmodule
--- FSqrt/fsqrt_testbench.sv
+++ FSqrt/fsqrt_testbench.sv
@@ -13,63 +13,6 @@ logic clk;
 
 fsqrt u0(clk,src,dest);
 
-// DEBUG:
-wire [31:0] s, d;
-wire [31:0] wire_s1, wire_s2, wire_s3, wire_s4;
-wire [63:0] wire_target1, wire_target2, wire_target3, wire_target4;
-wire [63:0] wire_b1, wire_b2, wire_b3, wire_b4;
-wire [63:0] wire_c1, wire_c2, wire_c3, wire_c4;
-wire [63:0] wire_d1, wire_d2, wire_d3, wire_d4;
-wire [63:0] wire_x1, wire_x2, wire_x3, wire_x4;
-reg [31:0] reg_s1, reg_s2, reg_s3, reg_s4;
-reg [63:0] reg_target1, reg_target2, reg_target3, reg_target4;
-reg [63:0] reg_b1, reg_b2, reg_b3, reg_b4;
-reg [63:0] reg_c1, reg_c2, reg_c3, reg_c4;
-reg [63:0] reg_d1, reg_d2, reg_d3, reg_d4;
-reg [63:0] reg_x1, reg_x2, reg_x3, reg_x4; 
-fsqrt_stage1 u1(s,wire_target1,wire_b1,wire_c1,wire_d1);
-fsqrt_stage2 u2(wire_target2,wire_b2,wire_c2,wire_d2,wire_x2);
-fsqrt_stage3 u3(wire_target3,wire_x3,wire_b3,wire_c3,wire_d3);
-fsqrt_stage4 u4(wire_s4,wire_target4,wire_b4,wire_c4,wire_d4,d);
-
-assign s = src;
-assign wire_s1 = s;
-assign wire_s2 = reg_s2;
-assign wire_s3 = reg_s3;
-assign wire_s4 = reg_s4;
-assign wire_target2 = reg_target2;
-assign wire_target3 = reg_target3;
-assign wire_target4 = reg_target4;
-assign wire_b2 = reg_b2;
-assign wire_b4 = reg_b4;
-assign wire_c2 = reg_c2;
-assign wire_c4 = reg_c4;
-assign wire_d2 = reg_d2;
-assign wire_d4 = reg_d4;
-assign wire_x3 = reg_x3;
-
-always @(posedge clk) begin
-  reg_s2 <= wire_s1;
-end
-always @(posedge clk) begin
-  reg_s3 <= wire_s2;
-end
-always @(posedge clk) begin
-  reg_s4 <= wire_s3;
-end
-always @(posedge clk) begin
-  reg_target2 <= wire_target1;
-  reg_target3 <= wire_target2;
-  reg_target4 <= wire_target3;
-  reg_b2 <= wire_b1;
-  reg_c2 <= wire_c1;
-  reg_d2 <= wire_d1;
-  reg_x3 <= wire_x2;
-  reg_b4 <= wire_b3;
-  reg_c4 <= wire_c3;
-  reg_d4 <= wire_d3;
-end
-
 // NOTE: wireをlogicにつないでおき、initial文の中でlogicに代入する
 assign src = src_logic;
 assign ans = ans_logic;
@@ -94,6 +37,12 @@ initial begin
       clk = !clk; #1; clk = !clk; #1;
       // NOTE: clock 4
       clk = !clk; #1; clk = !clk; #1;
+      // NOTE: clock 5
+      clk = !clk; #1; clk = !clk; #1;
+      // NOTE: clock 6
+      clk = !clk; #1; clk = !clk; #1;
+      // NOTE: clock 7
+      clk = !clk; #1; clk = !clk; #1;
 
     ans_real = $sqrt(src_real);
     ans_logic = $shortrealtobits(ans_real);
@@ -103,22 +52,12 @@ initial begin
     dest_real = $bitstoshortreal(dest);
 
     // NOTE: DEBUG:のために表示する
-    // if (ans != dest && src[31:31] == 0) begin
+    if (ans != dest && src[31:31] == 0) begin
       $display(" src = %e %b %b %b", src_real, src[31:31], src[30:23], src[22:0]);
       $display("dest = %e %b %b %b", dest_real, dest[31:31], dest[30:23], dest[22:0]);
       $display(" ans = %e %b %b %b", ans_real, ans[31:31], ans[30:23], ans[22:0]);
-      $display("x1 = %b", wire_x1);
-      $display("x2 = %b", wire_x2);
-      $display("x3 = %b", wire_x3);
-      $display("x4 = %b", wire_x4);
-      $display("target4 = %b", wire_target4);
-      $display("b4 = %b", wire_b4);
-      $display("c4 = %b", wire_c4);
-      $display("d4 = %b", wire_d4);
-      $display("x3 = %b", wire_x3);
-      $display("d = %b", d);
       $display();
-    // end
+    end
 
   end
 
